5223d170d20042549b68e3e4b7e061b6ce2904d0
Plinio Gatto <plinio.gatto@gmail.com>
2019-05-11 01:05:54 +0200

    Update StopServices.ps1

--- StopServices.ps1
+++ StopServices.ps1
@@ -1,11 +1,11 @@
 # Find service to stop using: Get-Service | Where {$_.status –eq 'running'}
 
-$myList = 'SysMain', 'wuauserv', 'AdobeARMservice', 'dbupdate', 'dbupdatem'
-foreach ($element in $myList) {
-	Write-Output "Stopping $element..."
-	Get-Service $element | Where {$_.status -eq 'Running'} | Stop-Service #Ottimizzazione avvio
+$services = 'SysMain', 'wuauserv', 'AdobeARMservice', 'dbupdate', 'dbupdatem'
+foreach ($service in $services) {
+	Write-Output "Stopping $service..."
+	Get-Service $service | Where {$_.status -eq 'Running'} | Stop-Service #Ottimizzazione avvio
 	if($?) {
-		Write-Output "$element stopped"
+		Write-Output "$service stopped"
 	}
 }
 
